a144075f258864b98e2c8546d4945ca8be3ee2c4
anandarizkyrm <rizky.pky@gmail.com>
2022-09-11 12:12:39 +0700

    cors fixing

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const  { ApolloServer } = require('apollo-server');
+const  { ApolloServer } = require('apollo-server-express');
+// const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core');
+const express = require('express');
+const http = require('http');
 const {typeDefs} = require('./schema')
 const { Query } = require('./resolver/Query')
 const { Tester } = require('./resolver/Tester')
@@ -8,8 +11,44 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 
+const app = express()
+// const server = new ApolloServer({
+//     typeDefs, 
+//     resolvers : {
+//       Query,
+//       Tester
+//     },
+//     csrfPrevention: true,
+//     cache: "bounded",
+//     cors: {
+//       origin: ["http://localhost:3000", "https://footballappgraphqlserver-production.up.railway.app"]
+//     },
+//     // plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+//     context : {
+//       db
+//     }
+// })
 
-const server = new ApolloServer({
+// const corsOptions = {
+//   origin: 'http://localhost:3000',
+//   credentials: true
+// }
+// server.start().then(()=>
+// {server.applyMiddleware({ app, cors: corsOptions });
+// })
+
+// server.listen().then(({url})=>{
+//   console.log("Server Run on " + url)
+// })
+
+
+
+async function startApolloServer(typeDefs, resolvers) {
+  const app = express();
+
+  const httpServer = http.createServer(app);
+
+  const server = new ApolloServer({
     typeDefs, 
     resolvers : {
       Query,
@@ -18,13 +57,27 @@ const server = new ApolloServer({
     csrfPrevention: true,
     cache: "bounded",
     cors: {
-      origin: ["http://localhost:3000", "https://footballappgraphqlserver-production.up.railway.app/"]
+      origin: ["http://localhost:3000", "https://footballappgraphqlserver-production.up.railway.app"]
     },
+    // plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
     context : {
       db
     }
-})
+  });
 
-server.listen().then(({url})=>{
-  console.log("Server Run on " + url)
-})
+  await server.start();
+
+
+  const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true
+  }
+  server.applyMiddleware({ app , cors : corsOptions});
+
+  await new Promise(resolve => httpServer.listen({ port: 4000 }, resolve));
+
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+}
+
+
+startApolloServer(typeDefs, {Query, Tester})
